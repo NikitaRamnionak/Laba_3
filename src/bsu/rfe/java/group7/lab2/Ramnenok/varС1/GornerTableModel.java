f630c04ffb984f9bc635372e74643f406f0c76e3
Nikita Ramnjonok <92184855+NikitaRamnionak@users.noreply.github.com>
2021-12-11 23:30:52 +0300

    Update GornerTableModel.java

--- src/bsu/rfe/java/group7/lab2/Ramnenok/varС1/GornerTableModel.java
+++ src/bsu/rfe/java/group7/lab2/Ramnenok/varС1/GornerTableModel.java
@@ -30,4 +30,25 @@ public class GornerTableModel extends AbstractTableModel {
     public int getRowCount() {
         return new Double(Math.ceil((to-from)/step)).intValue()+1;
     }
+
+    public String getColumnName(int col) {
+        switch (col) {
+            case 0:
+// Название 1-го столбца
+                return "Значение X";
+            case 1:
+// Название 2-го столбца
+                return "Значение многочлена";
+            case 2:
+                return "Разностороннее";
+            default:
+                return "";
+        }
+    }
+
+    public Class<?> getColumnClass(int col) {
+// И в 1-ом и во 2-ом столбце находятся значения типа Double
+        if(col==2) return Boolean.class;
+        else return Double.class;
+    }
 }
