754ed8289bc29b542542bc12787b7fa2471b061f
Nikita Ramnjonok <92184855+NikitaRamnionak@users.noreply.github.com>
2021-12-11 23:28:38 +0300

    Create GornerTableModel.java

---
+++ src/bsu/rfe/java/group7/lab2/Ramnenok/varС1/GornerTableModel.java
@@ -0,0 +1,33 @@
+package bsu.rfe.java.group7.lab2.Ramnenok.varС1;
+
+import javax.swing.table.AbstractTableModel;
+
+public class GornerTableModel extends AbstractTableModel {
+    private Double[] coefficients;
+    private Double from;
+    private Double to;
+    private Double step;
+
+    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients) {
+        this.from = from;
+        this.to = to;
+        this.step = step;
+        this.coefficients = coefficients;
+    }
+
+    public Double getFrom() {
+        return from;
+    }
+    public Double getTo() {
+        return to;
+    }
+    public Double getStep() {
+        return step;
+    }
+    public int getColumnCount() {
+        return 3;
+    }
+    public int getRowCount() {
+        return new Double(Math.ceil((to-from)/step)).intValue()+1;
+    }
+}
