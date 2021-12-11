fd5d11e591f161e8b646788672aff252f6c67e91
Nikita Ramnjonok <92184855+NikitaRamnionak@users.noreply.github.com>
2021-12-11 23:45:01 +0300

    Add data fields

--- src/bsu/rfe/java/group7/lab2/Ramnenok/varС1/Main.java
+++ src/bsu/rfe/java/group7/lab2/Ramnenok/varС1/Main.java
@@ -1,6 +1,63 @@
 package bsu.rfe.java.group7.lab2.Ramnenok.varС1;
 
-public class Main {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+public class Main extends JFrame {
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+
+    private Double[] coefficients;
+
+    // Объект диалогового окна для выбора файлов
+// Компонент не создаѐтся изначально, т.к. может и не понадобиться
+// пользователю если тот не собирается сохранять данные в файл
+    private JFileChooser fileChooser = null;
+
+    // Элементы меню вынесены в поля данных класса, так как ими необходимо
+// манипулировать из разных мест
+    private JMenuItem saveToTextMenuItem;
+    private JMenuItem saveToGraphicsMenuItem;
+    private JMenuItem searchValueMenuItem;
+    private JMenuItem aboutProgram;
+
+    // Поля ввода для считывания значений переменных
+    private JTextField textFieldFrom;
+    private JTextField textFieldTo;
+    private JTextField textFieldStep;
+    private Box hBoxResult;
+
+    // Визуализатор ячеек таблицы
+    private GornerTableCellRenderer renderer = new GornerTableCellRenderer();
+
+    // Модель данных с результатами вычислений
+    private GornerTableModel data;
 
     public static void main(String[] args) {
 	// write your code here
